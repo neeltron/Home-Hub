26559b598f3bcfeb16fc3d8e06084bf8ff83ffae
Neel Adwani <nprosofficial@gmail.com>
2021-05-03 02:35:33 +0530

    Update tyfficalexit.php

--- homehub/tyfficalexit.php
+++ homehub/tyfficalexit.php
@@ -7,12 +7,14 @@ $resp2 = "";
 $myObj->air = array(0);
 $myObj->temperature = array(0);
 $myObj->humidity = array(0);
+$myObj->timestamp = array(0);
 if (mysqli_num_rows($result_side) > 0) {
 while($row = mysqli_fetch_assoc($result_side)) {
-	array_push($myObj->air, intval($row['air']));
-	array_push($myObj->temperature, intval($row['temperature']));
-	array_push($myObj->humidity, intval($row['humidity']));
-	$myJSON = json_encode($myObj);
+    array_push($myObj->air, intval($row['air']));
+    array_push($myObj->temperature, intval($row['temperature']));
+    array_push($myObj->humidity, intval($row['humidity']));
+    array_push($myObj->timestamp, $row['timestamp']);
+    $myJSON = json_encode($myObj);
 }
 }
 echo $myJSON;
